ef92838e11759a62f9f678e2a3ec8f358cc80f33
Cagri Yildiz <cagriyild@gmail.com>
2024-05-03 13:13:16 +0200

    Move custom keymap into the custom plugins folder

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', '<leader>pv', ':Ex<CR>', { desc = 'Exit file' })
+
 return {
   {
     {
